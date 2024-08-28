738313ca98c09a2bbd50f8d8448e0ba968decab7
Helloyunho <yunho050840@gmail.com>
2024-08-28 13:26:46 +0900

    Initial Commit

---
+++ PlayBar-Test/PlayBar_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayBar_TestApp.swift
+//  PlayBar-Test
+//
+//  Created by Helloyunho on 2024/8/28.
+//
+
+import SwiftUI
+
+@main
+struct PlayBar_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
